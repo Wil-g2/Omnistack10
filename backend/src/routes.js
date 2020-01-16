c8e0458a8ff7811e0914b969ce5847dfce472c61
Willian Rezende <willian.rezende@climatempo.com.br>
2020-01-15 23:51:16 -0300

    feat(Backend/Routes): added routes for application

---
+++ backend/src/routes.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const routes = Router();
+
+const DevController = require("./controllers/DevController");
+const SearchController = require("./controllers/SearchController");
+
+routes.get("/", (req, res) => {
+  return res.json({ msg: "Ok" });
+});
+
+routes.get("/devs", DevController.index);
+routes.post("/devs", DevController.store);
+routes.put("/devs", DevController.update);
+routes.delete("/devs", DevController.destroy);
+
+routes.get("/search", SearchController.index);
+module.exports = routes;
