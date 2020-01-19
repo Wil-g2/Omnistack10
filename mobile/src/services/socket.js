993db2f96fcf2f8c101f97225f9263a78237e269
wil-g2 <wil-g2@hotmail.com>
2020-01-19 14:50:32 -0300

    feat(Mobile): added sokcet.io-client configuration

---
+++ mobile/src/services/socket.js
@@ -0,0 +1,35 @@
+import socketio from 'socket.io-client'; 
+
+const socket = socketio('http://192.168.0.106:3333', {
+  autoConnect: false,
+}); 
+
+function subscribeTonewDevs(subscribeFunction) {
+  console.log('ok');
+  socket.on('newDev', subscribeFunction);
+  socket.on('newDev', (data) => {
+    console.log(data)
+  });
+};
+
+function connect(latitude, longitude, techs) {
+  socket.io.opts.query = {
+    latitude, 
+    longitude,
+    techs
+  };
+
+  socket.connect();
+};
+
+function disconnect(){
+  if(socket.connected) {
+    socket.disconnect();
+  };
+};
+
+export { 
+  connect, 
+  disconnect,
+  subscribeTonewDevs
+};
