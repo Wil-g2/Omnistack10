74de86ac638d5a1492567ae57c618ecb68b15652
wil-g2 <wil-g2@hotmail.com>
2020-01-19 14:44:42 -0300

    chore(Backend): added socket.io and dotenv configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,18 @@
+require('dotenv/config');
 const express = require("express");
-const app = express();
-const routes = require("./routes");
 const cors = require("cors");
+const http = require('http');
+
+const routes = require("./routes");
+const { setupWebsocket } = require('./websocket'); 
+const calculateDistance = require('./utils/calculateDistance'); 
+
+const app = express();
+const server = http.Server(app);
+setupWebsocket(server);
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/omnistack", {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useFindAndModify: true,
   useUnifiedTopology: true
@@ -14,4 +22,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
